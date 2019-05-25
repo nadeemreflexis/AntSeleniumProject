70352dede91a67662779dfe34396b951419a874c
nadeemreflexis <nadeem.zaya@gmail.com>
2019-05-25 22:13:48 +0530

    first version code2

--- src/ant/testng/ClassThree.java
+++ src/ant/testng/ClassThree.java
@@ -2,6 +2,7 @@ package ant.testng;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.SkipException;
 import org.testng.annotations.Test;
 
 public class ClassThree 
@@ -9,12 +10,12 @@ public class ClassThree
 	@Test
 	public void threeTest() throws InterruptedException
 		{
-		WebDriver driver=new ChromeDriver();
+		/*WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("http://rediff.com");
 		Thread.sleep(2000);
-		driver.quit();
-		 //throw new SkipException("third test case is skipped");
+		driver.quit();*/
+		 throw new SkipException("third test case is skipped");
 		
 		}
 
